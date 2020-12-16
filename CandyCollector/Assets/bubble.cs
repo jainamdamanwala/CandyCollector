43b41e4bb366919394b121edabe78cfc166f1f70
jainamdamanwala <54740079+jainamdamanwala@users.noreply.github.com>
2020-12-16 15:20:48 +0530

    jainam

--- CandyCollector/Assets/bubble.cs
+++ CandyCollector/Assets/bubble.cs
@@ -17,7 +17,17 @@ public class bubble : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
+            RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
+            if (hit.collider != null && hit.collider.tag == "Player")
+            {
+                player.gravityScale = 1f;
+            }
+        }
+
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
