5f10f5d93c4d20701031a51ed28386064c2906b1
jainamdamanwala <54740079+jainamdamanwala@users.noreply.github.com>
2020-12-18 11:41:36 +0530

    jainam3

--- CandyCollector/Assets/Scripts/Rope.cs
+++ CandyCollector/Assets/Scripts/Rope.cs
@@ -14,7 +14,22 @@ public class Rope : MonoBehaviour {
 		GenerateRope();
 	}
 
-	void GenerateRope ()
+    private void Update()
+    {
+		if (Input.GetMouseButtonDown(0))
+		{
+			Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
+			RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
+			Debug.Log(hit.collider);
+			if(hit.collider.tag == "Rope" && hit.collider != null)
+            {
+				this.transform.position = mousePos2D;
+            }
+		}
+	}
+
+    void GenerateRope ()
 	{
 		Rigidbody2D previousRB = hook;
 		for (int i = 0; i < links; i++)
---
+++ CandyCollector/Assets/Scripts/Slider.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slider : MonoBehaviour
+{
+    private float startPosX, startPosY;
+    private bool isBeingHeld = false;
+
+    void Update()
+    {
+        if(isBeingHeld == true)
+        {
+            Vector3 mousePos;
+            mousePos = Input.mousePosition;
+            mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+            this.gameObject.transform.localPosition = new Vector3(mousePos.x - startPosX, 0);
+        }
+    }
+    private void OnMouseDown()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePos;
+            mousePos = Input.mousePosition;
+            mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+
+            startPosX = mousePos.x - this.transform.localPosition.x;
+            startPosY = mousePos.y - this.transform.localPosition.y;
+
+            isBeingHeld = true;
+        }
+
+    }
+    private void OnMouseUp()
+    {
+
+        isBeingHeld = false;
+    }
+}
